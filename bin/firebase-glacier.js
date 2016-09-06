a7ceee1c61bb32fe56a36a798170de82792ca2e7
Manuel Rodriguez <jose93rd@gmail.com>
2016-09-06 14:16:58 -0700

    CLI init command

--- bin/firebase-glacier.js
+++ bin/firebase-glacier.js
@@ -1,8 +1,64 @@
 #!/usr/bin/env node
 
 var glacier = require('firebase-glacier')
+var path = require('path')
+var fs = require('fs')
 
-glacier.build([
-    glacier.middleware.decorators,
-    glacier.middleware.validate
-])
+var BEHAVIOR = 'BUILD'
+
+const run = () => {
+    process.argv.forEach((val) => {
+        if(val === 'init') {
+            BEHAVIOR = 'INIT'
+        }
+    })
+
+    if(BEHAVIOR === 'BUILD') {
+        glacier.build([
+            glacier.middleware.decorators,
+            glacier.middleware.validate
+        ])
+    }
+
+    if(BEHAVIOR === 'INIT') {
+        if(fs.existsSync(path.resolve('./glacier.yml'))) {
+            console.log('FORBIDDEN, glacier.yml already exists')
+        } else {
+            fs.writeFile(path.resolve('./glacier.yml'), init_template, 'utf8', (err) => {
+                if(err) {
+                    console.log(err)
+                }
+            })
+        }
+    }
+}
+
+var init_template =
+`glacier: "1.0"
+info:
+    version: "0.0.0"
+    title: none
+    description: none
+    termsOfService: none
+    license: none
+    config:
+        web:
+            apiKey: none
+            authDomain: none
+            databaseURL: none
+            storageBucket: none
+    glossary:
+auth:
+    methods:
+        - email
+        - google
+        - facebook
+        - twitter
+        - github
+        - anonymous
+    oauthRedirectDomains:
+        - localhost
+    allowMultipleAccountsWithSameEmail: false
+database:`
+
+run()
